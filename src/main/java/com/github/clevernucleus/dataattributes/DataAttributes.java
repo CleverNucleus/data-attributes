4c44df16b4f4d6a2147d9e35fdb67fcc0a903a88
CleverNucleus <jamie@mclean007.plus.com>
2021-10-08 13:29:03 +0100

    Fixed issue Fabric API
    *Fixed a dumb mistake I made by missing ">=" for versions of Fabric API, causing Data Attributes to not be compatible with anything other than 0.40.1+1.17

--- src/main/java/com/github/clevernucleus/dataattributes/DataAttributes.java
+++ src/main/java/com/github/clevernucleus/dataattributes/DataAttributes.java
@@ -30,7 +30,7 @@ public final class DataAttributes implements ModInitializer {
 	public static final LoaderJsonManager MANAGER = new LoaderJsonManager();
 	public static final Identifier SYNC = new Identifier(API.MODID, "sync");
 	/** Manual; ugh, I know. */
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "1.0.6";
 	
 	private static void loginQueryStart(ServerLoginNetworkHandler handler, MinecraftServer server, PacketSender sender, ServerLoginNetworking.LoginSynchronizer synchronizer) {
 		PacketByteBuf buf = PacketByteBufs.create();
