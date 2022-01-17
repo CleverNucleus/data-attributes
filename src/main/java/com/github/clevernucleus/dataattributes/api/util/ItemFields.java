1edf105e91e8047dc37c7c187e4f6403f9aa43e5
CleverNucleus <jamie@mclean007.plus.com>
2022-01-17 22:38:48 +0000

    Fixed ItemFields attack speed uuid
    *ItemFields#attackSpeedModifierID now correctly points to Item#ATTACK_SPEED_MODIFIER_ID.
    
    +Incremented version number
    This will be a new minor version but we'll hold off on releasing it because it contains such a small change for now, we'll wait to see if we can find more bugs.

--- src/main/java/com/github/clevernucleus/dataattributes/api/util/ItemFields.java
+++ src/main/java/com/github/clevernucleus/dataattributes/api/util/ItemFields.java
@@ -15,5 +15,5 @@ public final class ItemFields extends Item {
 	
 	public static UUID attackDamageModifierID() { return ATTACK_DAMAGE_MODIFIER_ID; }
 	
-	public static UUID attackSpeedModifierID() { return ATTACK_DAMAGE_MODIFIER_ID; }
+	public static UUID attackSpeedModifierID() { return ATTACK_SPEED_MODIFIER_ID; }
 }
